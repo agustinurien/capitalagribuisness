22984d95231818d5cff502b4753561bb866c2e3c
Agustín <agustin@MacBook-Pro-de-MacBookPro.local>
2025-07-17 11:09:17 -0300

    redirijir

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel";
 
-// https://astro.build/config
 export default defineConfig({
+  output: "server", // importante para middleware
+  adapter: vercel(), // sin opciones aquí
   integrations: [react()],
 });
